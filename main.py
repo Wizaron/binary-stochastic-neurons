d317edf25452fc423ec05f8c9f6dbc8299e21943
Onur Kaplan <onur@miletos.co>
2017-12-25 00:17:44 +0300

    nepochs = 100 as default.

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ parser.add_argument('--slope-annealing', action='store_true', default=False,
                     help='Use slope annealing trick')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
-parser.add_argument('--epochs', type=int, default=30, metavar='N',
-                    help='number of epochs to train (default: 30)')
+parser.add_argument('--epochs', type=int, default=100, metavar='N',
+                    help='number of epochs to train (default: 100)')
 parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
                     help='learning rate (default: 0.1)')
 parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
