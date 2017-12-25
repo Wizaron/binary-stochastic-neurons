ea095315617bd6dfbf484ed2da251222cac85c8b
Onur Kaplan <onur@miletos.co>
2017-12-25 09:41:35 +0300

    Print results as a markdown table.

--- parse_logs.py
+++ parse_logs.py
@@ -4,11 +4,12 @@ import numpy as np
 logs = glob.glob(os.path.join('logs', '*.log'))
 logs = np.sort(logs)
 
-print '{:40}:{:6}:{}'.format('Model', 'Epoch', 'Accuracy')
+print '| {:40} | {:6} | {:8} |'.format('Model', 'Epoch', 'Accuracy')
+print '|:{:40}:|:{:6}:|:{:8}:|'.format('-' * 40, '-' * 6, '-' * 8)
 for log_file in logs:
     log = np.loadtxt(log_file, dtype='str', delimiter=',')[1:]
     best = log[np.argmax(log[:, 3])]
     best_epoch, best_acc = best[[0, 3]]
     log_name = os.path.splitext(os.path.basename(log_file))[0]
-    out = '{:40}:{:6}:{}'.format(log_name, best_epoch, best_acc)
+    out = '| {:40} | {:6} | {:8} |'.format(log_name, best_epoch, best_acc)
     print out
